51ae8f3d51506d0289f2d7c736dcb7203a2fb0d8
Amritpal Singh <amrit3701@gmail.com>
2023-03-26 19:59:56 +0530

    Implemented logger

--- fc_worker/__init__.py
+++ fc_worker/__init__.py
@@ -1,4 +1,15 @@
 """FreeCAD Worker"""
+import logging.config
+import pathlib
+
+_DIR = pathlib.Path(__file__).parent
+LOGGER_CONFIG = _DIR / "logger.config"
+
+logging.config.fileConfig(str(LOGGER_CONFIG))
+logger = logging.getLogger(__name__)
+logger.info("Imports successful!")
+
+
 from fc_worker.model_configurer import model_configurer_command
 
 __all__ = [model_configurer]
